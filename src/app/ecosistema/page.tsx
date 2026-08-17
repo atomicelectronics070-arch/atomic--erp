69491f14227ea4fec1de84a8c355a93971f7915a
Santiago <santiago@atomic.com>
2026-08-17 11:06:47 +0200

    Feature: Exponer ruta publica /ecosistema para el Gráfico 3D Interactivo Galaxia (Ecosistema ATOMIC con Afiliados, Vendedor, Miembros Oficiales y Estudiante/Académico)

---
+++ src/app/ecosistema/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+import PageClient from "../dashboard/ecosistema-tomc/PageClient"
+
+export default function PublicEcosistemaPage() {
+    return <PageClient />
+}
