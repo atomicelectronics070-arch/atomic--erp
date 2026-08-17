a67ac8fa450cec5fdee6993895dbfb3c2910caeb
Santiago <santiago@atomic.com>
2026-08-17 11:44:18 +0200

    Revert: Dejar ATOMIC 100% limpio en su estado original sin rutas ni archivos externos

--- src/app/ecosistema/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
-import PageClient from "../dashboard/ecosistema-tomc/PageClient"
-
-export default function PublicEcosistemaPage() {
-    return <PageClient />
-}
