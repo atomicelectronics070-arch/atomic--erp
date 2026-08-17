a793e4deb11ac9862970c2a0e3f15400fbd659aa
Santiago <santiago@atomic.com>
2026-08-17 11:39:00 +0200

    Feature: Expandir Grafico 3D Galaxia interactivo con nodos Atomic Migración, Landing de Ventas, Marketing V2, Socios & Alianzas en /soft3 y /ecosistema

--- src/app/soft3/page.tsx
+++ src/app/soft3/page.tsx
@@ -1,14 +1,8 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
+import PageClient from "../dashboard/ecosistema-tomc/PageClient"
+
 export default function Soft3Page() {
-  return (
-    <div className="w-screen h-screen bg-[#030712] overflow-hidden">
-      <iframe
-        src="/soft3.html"
-        className="w-full h-full border-none"
-        title="Cerebro SOFT3 Sistemas Core"
-      />
-    </div>
-  )
+    return <PageClient />
 }
